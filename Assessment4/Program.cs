66bcc0cbf439efa778f36322c5b519b34b398fa1
Rakesh <126458499+rakeshET@users.noreply.github.com>
2024-10-21 16:20:10 +0530

    Add files via upload
    Create a text processing application that:
    
    Accepts a paragraph of text from the user
    Counts the number of words, sentences, and characters
    Identifies and displays the longest word
    Reverses each word while maintaining sentence structure
    Uses StringBuilder where appropriate for better performance

---
+++ Assessment4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter string:");
+            string text = Console.ReadLine();
+            Console.WriteLine("You entered :"+text);
+
+            // WORDS COUNT
+
+            int wordsCount = text.Trim(' ').Split(' ').Length;
+            Console.WriteLine("Words Count :{0}",wordsCount);
+
+            //SENTENCE COUNT
+
+            int sentenceCount = text.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            Console.WriteLine("Sentence Count :{0}",sentenceCount);
+
+            //CHARACTERS COUNT
+            int charCount = text.Length;
+            Console.WriteLine("Characters Count :{0}", charCount);
+
+            //LONGEST WORD
+            string[] words = text.Split(new[] { " " }, StringSplitOptions.None);
+
+            string longestWord = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    longestWord = s;
+                    ctr = s.Length;
+                }
+            }
+                
+            Console.WriteLine(longestWord);
+        }
+    }
+}
+
