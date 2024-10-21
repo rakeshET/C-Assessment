5ea151444234613633e2f709c881e3568d13326b
Rakesh <126458499+rakeshET@users.noreply.github.com>
2024-10-21 12:47:08 +0530

    Add files via upload
    Design a simple banking system that:
    
    Create a BankAccount class with properties for account number, balance, and account holder name
    Implement methods for deposit and withdrawal
    Ensure the balance cannot go below zero
    Add a method to display the account details
    Include proper validation for all operations

---
+++ Assessment2/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Assessment2
+{
+    public class BankAccocunt
+    {
+        public String AccountHolder;
+        public long AccountNumber;
+        public double DepositAmount;
+        public double WithdrawalAmount;
+        public Double AccountBalance = 1000;
+
+        public void AccountDeposit()
+        {
+            if (DepositAmount > 0)
+            {
+                AccountBalance += DepositAmount;
+                Console.WriteLine("Name :{0}\n," +
+                    "AccountNumber : {1}\n," +
+                    "Account Balance :{2}", AccountHolder, AccountNumber, AccountBalance);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Deposit Amount");
+            }
+        }
+        public void AccountWithdraw()
+        {
+            if (WithdrawalAmount > 0 && WithdrawalAmount <= AccountBalance)
+            {
+                Console.WriteLine("Name :{0}\n," +
+                    "AccountNumber : {1}\n," +
+                    "Account Balance :{2}", AccountHolder, AccountNumber, AccountBalance);
+            }
+            else
+            {
+                Console.WriteLine("No sufficient balance");
+            }
+        }
+
+
+
+    }
+}
---
+++ Assessment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Assessment2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BankAccocunt account = new BankAccocunt();
+            Console.WriteLine("Enter name");
+            account.AccountHolder = Console.ReadLine();
+            Console.WriteLine("Enter Account number");
+            account.AccountNumber = Int64.Parse(Console.ReadLine());
+            Console.WriteLine("deposit or withdrawal?");
+
+            String option = Console.ReadLine();
+            if (option == "deposit")
+            {
+                Console.WriteLine("Enter deposit amount");
+                account.DepositAmount = double.Parse(Console.ReadLine());
+                account.AccountDeposit();
+            }
+            if(option == "withdrawal")
+            {
+                Console.WriteLine("Enter withdrawal amount");
+                account.WithdrawalAmount = double.Parse(Console.ReadLine());
+                account.AccountWithdraw();
+
+            }
+
+        }
+    }
+}
