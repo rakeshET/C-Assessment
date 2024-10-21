bc374c9152135a37f1c773fd258307bcfcbab371
Rakesh <126458499+rakeshET@users.noreply.github.com>
2024-10-21 10:50:48 +0530

    Add files via upload
    Assessment 1:- 
    Task 1: Variables and Calculations
    Create a program that calculates an employee's weekly pay. The program should:
    
    Ask the user to input the hourly rate and hours worked
    Calculate regular pay for the first 40 hours
    Calculate overtime pay (1.5× regular rate) for any hours over 40
    Display the gross pay, tax (20% of gross), and net pay
    Handle invalid inputs appropriately (negative numbers, non-numeric input)

---
+++ Assessment1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your hourly rate :-");
+            var hourlyRate = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Hours worked");
+
+            var hoursWorked = int.Parse(Console.ReadLine());
+            var extraHours = (hoursWorked - 40);
+
+            if (hoursWorked >= 40)
+            {
+                Console.WriteLine("Extra worked hours : {0}" ,extraHours);
+                var overTimePay = extraHours * (hourlyRate * 1.5);
+                Console.WriteLine("Over time pay = {0}",overTimePay);
+
+            }
+            else
+            {
+                Console.WriteLine("You have to work atleast 40 hours");
+            }
+
+            var regularPay = hourlyRate * 40;
+            Console.WriteLine("Regular pay : {0}", regularPay);
+
+
+
+            var grossPay = hourlyRate * hoursWorked;
+            var netPay = ((hourlyRate * hoursWorked) - (grossPay/20));
+
+
+            Console.WriteLine("Net pay = {0}",netPay);
+            Console.WriteLine("Gross pay = {0}",grossPay);
+
+
+
+
+
+        }
+    }
+}
